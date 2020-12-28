c8ab9fa8ab0a32204d9f047fb618b0ddea3f7ba4
wiflore <williamflorezmaestre@gmail.com>
2020-12-28 08:03:13 -0500

    model tuning, artifact and insights"

--- src/utilities/visuals.py
+++ src/utilities/visuals.py
@@ -48,7 +48,7 @@ def distribution(data, transformed = False):
     fig.show()
 
 
-def evaluate(results, accuracy, f1, size = (15, 15)):
+def evaluate(results, accuracy, f1, size = (12, 12)):
     """
     Visualization code to display results of various learners.
     
@@ -61,7 +61,7 @@ def evaluate(results, accuracy, f1, size = (15, 15)):
   
     # Create figure
     fig, ax = pl.subplots(2, 3, figsize = size)
-    
+
 
     # Constants
     bar_width = 0.3
@@ -69,7 +69,7 @@ def evaluate(results, accuracy, f1, size = (15, 15)):
     
     # Super loop to plot four panels of data
     for k, learner in enumerate(results.keys()):
-        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
+        for j, metric in enumerate(['train_time', 'roc_auc_train', 'f_train', 'pred_time', 'roc_auc_test', 'f_test']):
             for i in np.arange(3):
                 
                 # Creative plot code
@@ -81,18 +81,18 @@ def evaluate(results, accuracy, f1, size = (15, 15)):
     
     # Add unique y-labels
     ax[0, 0].set_ylabel("Time (in seconds)")
-    ax[0, 1].set_ylabel("Accuracy Score")
+    ax[0, 1].set_ylabel("Roc_auc Score")
     ax[0, 2].set_ylabel("F-score")
     ax[1, 0].set_ylabel("Time (in seconds)")
-    ax[1, 1].set_ylabel("Accuracy Score")
+    ax[1, 1].set_ylabel("Roc_auc Score")
     ax[1, 2].set_ylabel("F-score")
     
     # Add titles
     ax[0, 0].set_title("Model Training")
-    ax[0, 1].set_title("Accuracy Score on Training Subset")
+    ax[0, 1].set_title("Roc_auc Score on Training Subset")
     ax[0, 2].set_title("F-score on Training Subset")
     ax[1, 0].set_title("Model Predicting")
-    ax[1, 1].set_title("Accuracy Score on Testing Set")
+    ax[1, 1].set_title("Roc_auc Score on Testing Set")
     ax[1, 2].set_title("F-score on Testing Set")
     
     # Add horizontal lines for naive predictors
